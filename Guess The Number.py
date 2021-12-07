084e6a189d9f23c58ee68d98c3ef7a3b7e3b97ab
Nifemi-Adewusi <92606720+Nifemi-Adewusi@users.noreply.github.com>
2021-12-06 18:40:51 -0800

    Update Guess The Number.py

--- Guess The Number.py
+++ Guess The Number.py
@@ -1,13 +1,13 @@
 import random
-number = random.randrange(1, 30)
-guess = int(input("Enter A Number Between 1 And 30: "))
+number = random.randrange(1, 20)
+guess = int(input("Enter A Number Between 1 And 20: "))
 while guess != number:
     if guess < number:
         print("You Need To Guess Higher")
-        guess = int(input("Enter A Number Between 1 And 30: "))
+        guess = int(input("Enter A Number Between 1 And 20: "))
     elif guess > number:
         print("You Need To Guess Lower")
-        guess = int(input("Enter A Number Between 1 And 30: "))
+        guess = int(input("Enter A Number Between 1 And 20: "))
 else:
         print(f'{number} is The Lucky Number!')
 
